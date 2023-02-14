9519501e51cb2e9603d930a3b9f8f2599bd21c5d
Olufemi Oluoje <lufemi.oluoje@54gene.com>
2023-02-14 13:42:22 +0100

    tables migration and model

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function achievements()
+    {
+        return $this->belongsToMany(Achievement::class)->withTimestamps();
+    }
+
+    public function badges()
+    {
+        return $this->belongsToMany(Badge::class)->withTimestamps();
+    }
 }
